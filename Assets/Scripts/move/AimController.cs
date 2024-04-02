b25f9b6537180151dd9e1e2a00d563fdba1abb58
wny0320 <33829442+wny0320@users.noreply.github.com>
2024-04-02 23:28:06 +0900

    플레이어 움직임 관련 피드백

--- Assets/Scripts/move/AimController.cs
+++ Assets/Scripts/move/AimController.cs
@@ -4,24 +4,60 @@ using UnityEngine;
 
 public class AimController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // 이렇게 쓰면 유니티 내부에서 데이터를 편집 할 수 있으며, Range는 해당 범위 내에서 실린더를 통해 조절 가능함
+    [SerializeField, Range(0,1)]
+    private float aimSpeed;
+    private bool cursorLockFlag = false;
+    private bool cursorVisibleFlag = false;
 
-    // Update is called once per frame
-    void Update()
+    private void mouseCursorTracking()
     {
         // 마우스 커서 위치를 따라 이동하도록 
         //  ScreenToWorld 는 게임창에서 마우스 위치를 인식하게 해준다
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        
+
         mousePos.z = 0;
+        // 함수 내부에서만 쓸 변수가 아니라면 선언은 함수 외부에서 선언
+        //float aimSpeed = 0.8f; //  에임이 마우스 커서를 따라오는 속도
+        transform.position = Vector3.Lerp(transform.position, mousePos, aimSpeed);
+    }
+    /// <summary>
+    /// 마우스가 창 밖으로 나가지 못하게 하는 함수, 왼쪽 Ctrl키로 on/off
+    /// </summary>
+    private void mouseCursorLock()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftControl))
+            cursorLockFlag = !cursorLockFlag;
+        if(cursorLockFlag == true)
+            Cursor.lockState = CursorLockMode.Confined;
+        else if(cursorLockFlag == false)
+            Cursor.lockState = CursorLockMode.None;
+    }
+    /// <summary>
+    /// 마우스 커서를 숨기는 함수, 왼쪽 Alt키로 on/off
+    /// </summary>
+    private void mouseCursorVisible()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftAlt))
+            cursorVisibleFlag = !cursorVisibleFlag;
+        if (cursorVisibleFlag == true)
+            Cursor.visible = true;
+        else if (cursorLockFlag == false)
+            Cursor.visible = false;
+    }
+    // 필요없는 이벤트 함수라면 꼭 없앨것
+    // Start is called before the first frame update
+    //void Start()
+    //{
+        
+    //}
 
-        float AimSpeed = 0.8f; //  에임이 마우스 커서를 따라오는 속도
-        transform.position = Vector3.Lerp(transform.position, mousePos, AimSpeed);
-
-;
+    // Update is called once per frame
+    void Update()
+    {
+        //최대한 업데이트 내부에서 코드짜지 말고 함수로 외부로 뺄 것
+        mouseCursorTracking();
+        mouseCursorLock();
+        mouseCursorVisible();
     }
 }
--- Assets/Scripts/move/PlayerController.cs
+++ Assets/Scripts/move/PlayerController.cs
@@ -5,12 +5,29 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rigid2D;
-    float WalkSpeed = 6.0f;              // 걷는 속도
+    // 코딩 스타일이고 회사마다 다르긴 한데 보통 private인 변수는 첫글자 소문자, public은 첫글자 대문자를 주로 씁니다
+    [SerializeField, Range(0,10)] // 마우스와 마찬가지
+    float walkSpeed = 6.0f;              // 걷는 속도
 
     // 애니메이션
     //Animator animator;  
 
+    // 여기는 스타일마다 달라서 함수라면 모두 대문자 쓰시는 분들이 있고,
+    // 변수와 마찬가지로 private라면 소문자 public이라면 대문자 쓰시는 분들 있어서 이건 취향입니다
+    private void playerMove()
+    {
+        // 좌우 앞뒤 이동
 
+        float inputY = 0;
+        float inputX = 0;
+        if (Input.GetKey(KeyCode.W)) inputY = 1;
+        if (Input.GetKey(KeyCode.S)) inputY = -1;
+        if (Input.GetKey(KeyCode.D)) inputX = 1;
+        if (Input.GetKey(KeyCode.A)) inputX = -1;
+
+
+        transform.Translate(new Vector2(inputX, inputY) * Time.deltaTime * walkSpeed);  // 키보드에 따른 방향으로 이동속도 만큼 이동 
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -25,21 +42,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-        // 좌우 앞뒤 이동
-        
-        float inputY = 0;
-        float inputX = 0 ;
-        if (Input.GetKey(KeyCode.W)) inputY = 1;
-        if (Input.GetKey(KeyCode.S)) inputY = -1;
-        if (Input.GetKey(KeyCode.D)) inputX = 1;
-        if (Input.GetKey(KeyCode.A)) inputX = -1;
-          
-
-        transform.Translate(new Vector2(inputX, inputY) * Time.deltaTime * WalkSpeed);  // 키보드에 따른 방향으로 이동속도 만큼 이동 
-
-
-
-
+        //마찬가지로 함수로 만들어서 업데이트 함수 안에 넣어주세요
+        playerMove();
     }
 }
