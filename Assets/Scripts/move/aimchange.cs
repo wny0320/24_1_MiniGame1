14b741e479ae2c23fa11b5a501c26fe7de2494c8
parkkumsan <ajhk1200@naver.com>
2024-07-10 14:52:51 +0900

    무기변경
    초반 기획안에 있던 r키를 누르면 에임이 변경 되도록 만듬 하지만 현재 에임에 따라 총알이 나가는것이 아닌 마우스 커서에 따라 총알이 나감 고쳐야 함

---
+++ Assets/Scripts/move/aimchange.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aimchange : MonoBehaviour
+{
+
+    int totalWeapons = 1;
+    public int currentWeaponindex;
+
+    public GameObject[] guns;
+    public GameObject weaponHolder;
+    public GameObject currentGun; 
+
+    
+    void Start()
+    {
+        totalWeapons = weaponHolder.transform.childCount;   
+        guns = new GameObject[totalWeapons];   
+        
+        for (int i=0; i < totalWeapons; i++)
+        {
+            guns[i] = weaponHolder.transform.GetChild(i).gameObject;
+            guns[i].SetActive(false);
+        }
+
+        guns[0].SetActive(true);
+        currentGun = guns[0];
+        currentWeaponindex = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            if (currentWeaponindex < totalWeapons-1)
+            {
+                guns[currentWeaponindex].SetActive(false);
+                currentWeaponindex++;
+                guns[currentWeaponindex].SetActive(true);
+            }
+            else
+            {
+                guns[currentWeaponindex].SetActive(false);
+                currentWeaponindex = 0;
+                guns[currentWeaponindex].SetActive(true);
+            }
+        }
+    }
+}
