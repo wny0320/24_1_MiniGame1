276278f0fe968488f595f4e362738fa47deeb798
wny0320 <33829442+wny0320@users.noreply.github.com>
2024-07-17 17:10:12 +0900

    보스 패턴 관련
    플레이어 방향에 따른 Idle 상태 애니메이션 추가 및 패턴 애니메이션 베이스 작업

--- Assets/Scripts/Boss/BossController.cs
+++ Assets/Scripts/Boss/BossController.cs
@@ -7,6 +7,7 @@ public class BossController : BaseController
 {
     //해당 속성들이 보스 공통일지는 모름
     public NowBoss nowBoss;
+    public bool reduceDamageFlag = false;
     public float noDamagedTime = 0f;
     public bool phase2Flag = false;
     public float timeAdd = 0f;
@@ -21,6 +22,8 @@ public class BossController : BaseController
     public float patternTimer = 0f;
     // 단위 거리
     public float unitDis = 0.1f;
+    public const string X_DIR = "xDir";
+    public const string Y_DIR = "yDir";
     private void Awake()
     {
         rigid2D = GetComponent<Rigidbody2D>();
@@ -36,6 +39,7 @@ public class BossController : BaseController
     void Update()
     {
         stateMachine?.StateUpdateFunc();
+        SetAnimator();
         noDamagedTime += Time.deltaTime;
     }
 
@@ -67,10 +71,22 @@ public class BossController : BaseController
     public override void OnHit(float damage)
     {
         noDamagedTime = 0f;
+        if(reduceDamageFlag == true)
+        {
+            damage *= 0.7f;
+        }
+        stat.Hp -= damage;
     }
     public void PatternTimeAdder(float _time)
     {
         timeAdd = _time;
         timeAddFlag = true;
+    }
+    public void SetAnimator()
+    {
+        Transform playerTrans = Manager.Game.Player.transform;
+        Vector3 playerDir = Vector3.Normalize((Vector2)playerTrans.position - rigid2D.position);
+        animator.SetFloat(X_DIR, playerDir.x);
+        animator.SetFloat(Y_DIR, playerDir.y);
     }
 }
